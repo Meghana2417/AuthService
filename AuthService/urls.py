164611718b487ddad106c6ac41bc7f52f28b4c22
Meghana2417 <118287463+Meghana2417@users.noreply.github.com>
2025-11-17 12:06:18 +0530

    Update urls.py

--- AuthService/urls.py
+++ AuthService/urls.py
@@ -1,5 +1,16 @@
+#from django.urls import path, include
+
+#urlpatterns = [
+ #   path('api/auth/', include('authapp.urls')),
+#]
+from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Auth Service is running!")
 
 urlpatterns = [
-    path('api/auth/', include('authapp.urls')),
+    path("", home),                      #  Fix for "/"
+    path("api/auth/", include("accounts.urls")),   # your auth app
 ]
